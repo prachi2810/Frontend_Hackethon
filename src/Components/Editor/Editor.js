afca56399fa63b07a0f43b774cf9061054c31c71
Pratik Patil <p.patil7@zensar.com>
2023-03-02 14:28:34 +0530

    made changes in editor js

--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -75,8 +75,303 @@ const savePage=async()=>{
                                     defaults: 'auto',
                                     min: 0,
                                 },
+                                {
+                                    type: 'composite',
+                                    property: 'margin',
+                                    label: 'margin',
+                                    // Additional props
+                                    properties: [
+                                      { type: 'number', units: ['px'], default: '0', property: 'margin-top' },
+                                      { type: 'number', units: ['px'], default: '0', property: 'margin-right' },
+                                      { type: 'number', units: ['px'], default: '0', property: 'margin-bottom' },
+                                      { type: 'number', units: ['px'], default: '0', property: 'margin-left' },
+                                    ]
+                                  },
                             ],
                         },
+                        {
+                            name: 'Typography',
+                            open: false,
+                            properties: [
+                                {
+                                    type: 'select',
+                                    label: 'Font size',
+                                    property: 'font-size',
+                                    default: '1rem',
+                                    options: [
+                                      { id: '0.7rem', label: 'small' },
+                                      { id: '1rem', label: 'medium' },
+                                      { id: '1.2rem', label: 'large' },
+                                    ]
+                                  },
+                                  {
+                                    type: 'number',
+                                    label: 'letter spacing',
+                                    property: 'letter-spacing',
+                                    fixedSize:["normal","initial","inherit"],
+                                    units: ['px', '%', 'em', 'rem', 'vh', 'vw'],
+                                    min: 0,
+                                  },
+                                  {
+                                    type: 'number',
+                                    label: 'letter height',
+                                    property: 'letter-height',
+                                    fixedSize:["normal","initial","inherit"],
+                                    units: ['px', '%', 'em', 'rem', 'vh', 'vw'],
+                                    min: 0,
+                                  },
+                                  {
+                                    type: 'number',
+                                    label: 'Font size',
+                                    property: 'font-size',
+                                    units: ['px', '%', 'em', 'rem', 'vh', 'vw'],
+                                    min: 0,
+                                  },
+                                  {
+                                    type: 'select',
+                                    property:'font-family',
+                                    label: 'Font-Family',
+                                    default: "Arial, Helvetica, sans-serif",
+                                    // Additional props
+                                    options: [
+                                      {id: "Arial, Helvetica, sans-serif",label:"Arial"},
+                                      {id: 'Arial Black, Gadget, sans-serif', label: 'Arial Black'},
+                                      {id: 'Comic Sans MS, cursive, sans-serif', label: "Comic Sans MS"},
+                                      {id: "Brush Script MT, sans-serif", label: "Brush Script MT"},
+                                      {id: "Courier New, Courier, monospace", label: "Courier New"},
+                                      {id: "Georgia, serif", label: "Georgia"},
+                                      {id: "Helvetica, sans-serif", label: "Helvetica" },
+                                      {id: "Impact, Charcoal, sans-serif", label: "Impact"},
+                                      {id: "Lucida Sans Unicode, Lucida Grande, sans-serif", label: "Lucida Sans Unicode"},
+                                      {id: "Tahoma, Geneva, sans-serif", label: "Tahoma"},
+                                      {id: "Times New Roman, Times, serif", label: "Times New Roman"},
+                                      {id:"Trebuchet MS, Helvetica, sans-serif",label:"Trebuchet MS"},
+                                      {id:"Verdana, Geneva, sans-serif",label:"Verdana"}
+    
+                                    ]
+                                  },
+                                  {
+                                    type: 'select',
+                                    property:"font-weight",
+                                    label: 'Font-Weight',
+                                   
+                                    // Additional props
+                                    options: [
+                                      {id: "100",label: "Thin"},
+                                      {id: "200", label:"Extra-Light" },
+                                      {id: "300", label:"Light" },
+                                      {id:"400" , label:"Normal" },
+                                      {id: "500", label: "Medium"},
+                                      {id: "600", label:"Semi-Bold" },
+                                      {id: "700", label: "Bold"},
+                                      {id: "800", label: "Extra-Bold"},
+                                      {id:"900",label:"Ultra-Bold"
+                                    }
+                                     
+    
+                                    ]
+                                  },
+                            ],
+                        },
+                        {
+                            name: 'Color',
+                            open: false,
+                            type: 'stack',
+                            property: 'text-shadow',
+                            label: 'Stack type',
+                            // Additional props
+                            properties: [
+                              { type: 'number', units: ['px'], default: '0', property: 'x' },
+                              { type: 'number', units: ['px'], default: '0', property: 'y' },
+                              { type: 'number', units: ['px'], default: '0', property: 'blur' },
+                              { type: 'color', default: 'black', property: 'color' },
+                              {
+                                type:"file",
+                                label:"background-image",
+                                property:"background-image",
+                                functionName:"url",
+                                full:"",
+                                default:"none"
+                              },
+                              {
+                                type:"color",
+                                label:"background-color",
+                                property:"background-color",
+                                default:"none"
+                              },
+                              {
+                                type:"select",
+                                default:"scroll",
+                                label:"background-attachment",
+                                property:"background-color",
+                                options: [
+                                    { id:"scroll" },
+                                    { id:"fixed" },
+                                    { id:"local" },                                  
+                                  ]
+                              },
+    
+                            ]
+                          },
+                          {
+                            name: 'Decoration',
+                            open: false,
+                            // buildProps: ['width', 'min-height', 'padding'],
+                            properties: [
+                                {
+                                    type: 'select',
+                                   label:'align-content',
+                                    property: 'align-content',
+                                    default:"stretch",
+                                    properties: [
+                                        { id:"flex-start" },
+                                        { id:"flex-end" },
+                                        { id:"center" },
+                                        { id:"space-between" },
+                                        { id:"space-around" },    
+                                      ],
+                                      requires:[{display:"flex"}]
+                                },
+                                {
+                                    type: 'select',
+                                   label:'align-items',
+                                    property: 'align-items',
+                                    default:"stretch",
+                                    properties: [
+                                        { id:"flex-start" },
+                                        { id:"flex-end" },
+                                        { id:"center" },
+                                        { id:"baseline" },
+                                        { id:"stretch" },    
+                                      ],
+                                      requires:[{display:"flex"}]
+                                },
+                                {
+                                    type: 'radio',
+                                   label:'text-align',
+                                   default:"left",
+                                    property: 'text-align',
+                               
+                                    options: [
+                                        { id:"left" },
+                                        { id:"center" },
+                                        { id:"right" },
+                                        { id:"justify" },
+                                         
+                                      ]
+                                },
+                                {
+                                    type: 'radio',
+                                   label:'position',
+                                   default:"static",
+                                    property: 'position',
+                                   
+                                    options: [
+                                        { id:"static" },
+                                        { id:"relative" },
+                                        { id:"absolute" },
+                                        { id:"fixed" },
+                                         
+                                      ]
+                                },
+                               
+                                {
+                                    type: 'composite',
+                                    property: 'border',
+                                    label: 'border',
+                                    // Additional props
+                                    properties: [
+                                      { default: "0",id:"border-width-sub",min:0, property:"border-width" ,type:"number",units:['px','em', 'rem', 'vh', 'vw'] },
+                                      { default:"solid",id:"border-style-sub", property:"border-style"},
+                                      { default:"black",id:"border-color-sub",property:"border-color" ,type:"color" },
+                                     
+                                    ]
+                                  },
+                                  {
+                                    type: 'composite',
+                                    property: 'border-radius',
+                                    label: 'border-radius',
+                                    // Additional props
+                                    properties: [
+                                      { default: "0",id:"border-top-left-radius-sub",min:0, property:"border-top-left-radius" ,type:"number",units:['px','%','em', 'rem', 'vh', 'vw'] },
+                                      { default:"0",id:"border-top-right-radius-sub", property:"border-top-right-radius",type:"number",units:['px','%','em', 'rem', 'vh', 'vw']},
+                                      { default:"0",id:"border-bottom-right-radius-sub",property:"border-bottom-right-radius" ,type:"number",units:['px','%','em', 'rem', 'vh', 'vw'] },
+                                      { default:"0",id:"border-bottom-left-radius-sub", property:"border-bottom-left-radius",type:"number",units:['px','%','em', 'rem', 'vh', 'vw']},
+                                    ]
+                                  },
+                                  {
+                                    type: 'select',
+                                   label:'display',
+                                    property: 'display',
+                                    properties: [
+                                        { id:"block" },
+                                        { id:"inline" },
+                                        { id:"inline-block" },
+                                        { id:"flex" },
+                                        { id:"none" },    
+                                      ]
+                                },
+                                {
+                                    type: 'slider',
+                                   label:'opacity',
+                                    property: 'opacity',
+                                    default:"1",
+                                    max:1,
+                                    min:0,
+                                    step:0.01
+                                },
+                                 
+                                 
+                            ],
+                        },
+                        {
+                            name:"Flex",
+                            open: false,
+                            // buildProps: ['width', 'min-height', 'padding'],
+                            properties:[
+                                {
+                                    type: 'stack',
+                                    property: 'transition',
+                                    label: 'transition',
+                                    //formStyle:ƒ (t, e),
+                                    // Additional props
+                                    properties: [
+                                      { default: "width",id:"transition-property-sub",options:[{id:"all"},{id:"width"},{id:"height"},{id:"background-color"},{id:"transform"},{id:"box-shadow"},{id:"opacity"}] },
+                                      { default:"2s",id:"transition-duration-sub", min:0,property:"transition-duration",type:"number",units:['s','ms'] },
+                                      { default:"ease",id:"transition-timing-function-sub",options:[{id:"linear"},{id:"ease"},{id:"ease-in"},{id:"ease-out"},{id:"ease-in-out"}]}
+                                     
+                                    ]
+                                  },
+                                {
+                                    type: 'stack',
+                                    property: 'transition-property',
+                                    label: 'transition-duration',
+                                    default:"2s",
+                                    //formStyle:ƒ (t, e),
+                                    // Additional props
+                                    properties: "transition-duration",
+                                    type:"number",
+                                    units:['s','ms']
+                                },
+                                {
+                                    type: 'select',
+                                    property: '"transition-property"',
+                                    label: 'transition-property',
+                                    default:"width",
+                                    options:[
+                                        {id:"all"},
+                                        {id:"width"},
+                                        {id:"height"},
+                                        {id:"background-color"},
+                                        {id:"transform"},
+                                        {id:"box-shadow"},
+                                        {id:"opacity"},
+    
+                                    ]
+                                   
+                                },
+                            ]
+                          }
                     ],
                 },
                 blockManager: {
