06e4c732b033667400868aa07606e25c764b7bc1
prachi2810 <68736746+prachi2810@users.noreply.github.com>
2023-03-02 11:17:02 +0530

    login integrate

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import Editor from "./Components/Editor/Editor";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import Edit from "./Components/Edit/Edit";
-import Signincomponent from "./Components/login/Signincomponent";
+// import "../src/Components/Login/styles.css";
+import SignUpIncomponent from "./Components/Login/SignUpIncomponent";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="" index element={<Home />}></Route>
-        <Route path="login" index element={<Login />}></Route>
         <Route path="register" element={<Register />}></Route>
+        <Route path="login1" element={<Login />}></Route>
         <Route path="home" index element={<Home />}></Route>
         <Route path="editor" index element={<Editor />}></Route>
         <Route path="edit/:id" index element={<Edit />}></Route>
-        <Route path="addPage" index element={<Addpage />}></Route>
-        <Route path="login1" index element={<Signincomponent/>}></Route>
+        <Route path="addPage/:id" index element={<Addpage />}></Route>
+        <Route path="login" index element={<SignUpIncomponent/>}></Route>
       </Routes>
     </div>
   );
